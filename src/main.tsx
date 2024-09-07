01e1441fc10e500974b90cb042c6635d9457ee7d
Uditangshu <uditanshuchakraborty@gmail.com>
2024-09-07 23:08:14 +0530

    fixed file structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Component from './App'; 
-
+import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Component />} /> 
+        <Route path="/" element={<App />} /> 
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
